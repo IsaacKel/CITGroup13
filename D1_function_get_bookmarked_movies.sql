c05a05d01d4a7f1f3386dca4e04b76ce4cdd9fd9
Sebastianrix <82590812+Sebastianrix@users.noreply.github.com>
2024-09-30 17:08:55 +0200

    Created function : bookmark_movie
    Test with :
    SELECT * FROM get_bookmarked_movies(1);
    
    The parameters are (userId)

---
+++ D1_function_get_bookmarked_movies.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE FUNCTION "public"."get_bookmarked_movies"("p_userid" INT)
+  RETURNS TABLE("tconst" varchar, "title" varchar) AS $BODY$
+BEGIN
+    RETURN QUERY 
+    SELECT b.tconst, tb.primarytitle::VARCHAR
+    FROM userbookmarks b
+    JOIN title_basics tb ON b.tconst = tb.tconst
+    WHERE b.userid = p_userid;
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100
+  ROWS 1000
