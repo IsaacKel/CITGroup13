e971d5df92c72395552e4f0b5d98c9d3e1d58aa7
Sebastianrix <82590812+Sebastianrix@users.noreply.github.com>
2024-10-03 18:55:43 +0200

    Created D.12 function Best_match_query
    Test with :
    SELECT * FROM best_match_query(ARRAY['inception', 'dream']);

---
+++ D12_function_Best_match_query.sql
@@ -0,0 +1,20 @@
+CREATE OR REPLACE FUNCTION best_match_query(p_keywords TEXT[])
+RETURNS TABLE(tconst VARCHAR, title VARCHAR, match_count INT) AS $$
+BEGIN
+    RETURN QUERY
+    SELECT 
+        tb.tconst::VARCHAR,               
+        tb.primarytitle::VARCHAR,          
+        COUNT(DISTINCT wi.word)::INT AS match_count
+    FROM 
+        title_basics tb
+    JOIN 
+        wi ON tb.tconst = wi.tconst
+    WHERE 
+        wi.word = ANY(p_keywords)
+    GROUP BY 
+        tb.tconst, tb.primarytitle
+    ORDER BY 
+        match_count DESC;
+END;
+$$ LANGUAGE plpgsql;
