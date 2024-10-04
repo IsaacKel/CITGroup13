0b660a63a3650b0b3d7aa6c814cb71e3d5067d75
Sebastianrix <82590812+Sebastianrix@users.noreply.github.com>
2024-10-04 15:36:51 +0200

    Update D1_function_get_bookmarks.sql
    You can now both see tconst and nconst. AKA, both names and titles are listed then you view a users bookmark list.

--- D1_function_get_bookmarks.sql
+++ D1_function_get_bookmarks.sql
@@ -1,13 +1,33 @@
-CREATE OR REPLACE FUNCTION "public"."get_bookmarks"("p_userid" INT)
-  RETURNS TABLE("tconst" varchar, "title" varchar) AS $BODY$
+CREATE OR REPLACE FUNCTION "public"."get_bookmarks"("p_userid" int4)
+RETURNS TABLE(bookmark_type VARCHAR, id VARCHAR, name_or_title VARCHAR) AS $$
 BEGIN
     RETURN QUERY 
-    SELECT b.tconst, tb.primarytitle::VARCHAR
-    FROM userbookmarks b
-    JOIN title_basics tb ON b.tconst = tb.tconst
-    WHERE b.userid = p_userid;
+    -- Fetch bookmarked movies
+    SELECT 
+        'title'::VARCHAR AS bookmark_type,  -- Indicate this is a title bookmark, cast to VARCHAR
+        b.tconst::VARCHAR AS id,            -- Return the tconst (movie/series ID), cast to VARCHAR
+        tb.primarytitle::VARCHAR AS name_or_title  -- Return the movie/series title, cast to VARCHAR
+    FROM 
+        userbookmarks b
+    JOIN 
+        title_basics tb ON b.tconst = tb.tconst
+    WHERE 
+        b.userid = p_userid
+
+    UNION ALL
+
+    -- Fetch bookmarked actors/directors
+    SELECT 
+        'name'::VARCHAR AS bookmark_type,   -- Indicate this is a name bookmark, cast to VARCHAR
+        b.nconst::VARCHAR AS id,            -- Return the nconst (name ID), cast to VARCHAR
+        nb.primaryname::VARCHAR AS name_or_title  -- Return the person's name, cast to VARCHAR
+    FROM 
+        userbookmarks b
+    JOIN 
+        name_basics nb ON b.nconst = nb.nconst
+    WHERE 
+        b.userid = p_userid;
 END;
-$BODY$
-  LANGUAGE plpgsql VOLATILE
-  COST 100
-  ROWS 1000
+$$ LANGUAGE plpgsql VOLATILE
+COST 100
+ROWS 1000;
