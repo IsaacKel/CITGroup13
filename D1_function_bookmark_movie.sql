75a871e05e7038e5b978a67a1828fade8a8d6843
Sebastianrix <82590812+Sebastianrix@users.noreply.github.com>
2024-10-07 14:02:05 +0200

    Now check's if bookmark already exsists!
    Previously you could bookmark the same thing again and again.

--- D1_function_bookmark_movie.sql
+++ D1_function_bookmark_movie.sql
@@ -1,8 +1,18 @@
 CREATE OR REPLACE FUNCTION "public"."bookmark_movie"("p_userid" int4, "p_tconst" varchar)
-  RETURNS "pg_catalog"."void" AS $BODY$
+  RETURNS "pg_catalog"."void" AS
+$BODY$
 BEGIN
-    INSERT INTO userbookmarks (userid, tconst) VALUES (p_userid, p_tconst);
+    -- Check if the bookmark already exists
+    IF NOT EXISTS (
+        SELECT 1
+        FROM userbookmarks
+        WHERE userid = p_userid AND tconst = p_tconst
+    ) THEN
+        -- If not exists, insert the new bookmark
+        INSERT INTO userbookmarks (userid, tconst)
+        VALUES (p_userid, p_tconst);
+    END IF;
 END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
-  COST 100
+  COST 100;
