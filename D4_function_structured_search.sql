ec14bd3e13e335d44f7ae6cfcf892f26309f253e
Sebastianrix <82590812+Sebastianrix@users.noreply.github.com>
2024-10-07 13:26:36 +0200

    Updated to use the right tables.
    It was referencing old tables.

--- D4_function_structured_search.sql
+++ D4_function_structured_search.sql
@@ -3,13 +3,12 @@ CREATE OR REPLACE FUNCTION "public"."structured_string_search"("p_title" varchar
 BEGIN
     RETURN QUERY 
     SELECT tb.tconst::TEXT, tb.primarytitle::TEXT
-    FROM title_basics tb
-    JOIN omdb_data od ON tb.tconst = od.tconst
-    JOIN title_principals tp ON tb.tconst = tp.tconst
-    JOIN name_basics nb ON tp.nconst = nb.nconst
+    FROM titlebasic tb
+    JOIN titlecharacters tp ON tb.tconst = tp.tconst
+    JOIN namebasic nb ON tp.nconst = nb.nconst
     WHERE tb.primarytitle ILIKE '%' || p_title || '%'
-      AND od.plot ILIKE '%' || p_plot || '%'
-      AND tp.characters ILIKE '%' || p_characters || '%'
+      AND tb.plot ILIKE '%' || p_plot || '%'
+      AND tp.character ILIKE '%' || p_characters || '%'
       AND nb.primaryname ILIKE '%' || p_names || '%';
 END;
 $BODY$
