00b3bd1dd1529b1fe43984aa0477afca83ad05ef
Sebastianrix <82590812+Sebastianrix@users.noreply.github.com>
2024-10-07 14:03:21 +0200

    Now check's if bookmark already exsists!

--- D1_function_bookmark_name.sql
+++ D1_function_bookmark_name.sql
@@ -1,8 +1,18 @@
 CREATE OR REPLACE FUNCTION "public"."bookmark_name"("p_userid" int4, "p_nconst" varchar)
-  RETURNS "pg_catalog"."void" AS $BODY$
+  RETURNS "pg_catalog"."void" AS
+$BODY$
 BEGIN
-    INSERT INTO userbookmarks (userid, nconst) VALUES (p_userid, p_nconst);
+    -- Check if the bookmark for the name already exists
+    IF NOT EXISTS (
+        SELECT 1
+        FROM userbookmarks
+        WHERE userid = p_userid AND nconst = p_nconst
+    ) THEN
+        -- If not exists, insert the new bookmark
+        INSERT INTO userbookmarks (userid, nconst)
+        VALUES (p_userid, p_nconst);
+    END IF;
 END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
-  COST 100
+  COST 100;
