7d90b35d173d62a1c56e192176e3c3559eb307a0
Sebastianrix <82590812+Sebastianrix@users.noreply.github.com>
2024-10-07 14:06:56 +0200

    Made sure the search history doens't log twice if the user already searched the query before

--- D2_function_update_search_history.sql
+++ D2_function_update_search_history.sql
@@ -1,9 +1,18 @@
 CREATE OR REPLACE FUNCTION "public"."update_search_history"("p_userid" int4, "p_search_string" varchar)
-  RETURNS "pg_catalog"."void" AS $BODY$
+  RETURNS "pg_catalog"."void" AS
+$BODY$
 BEGIN
-    INSERT INTO userSearchHistory (userId, searchQuery, searchDate) 
-    VALUES (p_userid, p_search_string, CURRENT_DATE);
+    -- Check if the search query already exists for the user
+    IF NOT EXISTS (
+        SELECT 1
+        FROM userSearchHistory
+        WHERE userId = p_userid AND searchQuery = p_search_string
+    ) THEN
+        -- If not exists, insert the new search query
+        INSERT INTO userSearchHistory (userId, searchQuery, searchDate)
+        VALUES (p_userid, p_search_string, CURRENT_DATE);
+    END IF;
 END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
-  COST 100
+  COST 100;
